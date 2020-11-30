7f1bfdee8fa88ce9a9fae81b0c70f3225fa493c9
JJ Merelo <jjmerelo@gmail.com>
2020-11-30 11:09:45 +0100

    Changing preparing script

--- utils/prepare.sh
+++ utils/prepare.sh
@@ -4,4 +4,6 @@ rm demo.html CONTRIBUTING.md .npmignore package.json package-lock.json gulpfile.
 
 cat ../reveal.js-template/README-md-preffix.md README.md > README-new.md
 mv README-new.md README.md
+cp -r ../reveal.js-template/utils .
+cp ../reveal.js-template/extra.css dist
 rm -rf test examples dist/theme/fonts css
